3a77243740a9ed7748a22205970498174b1fed5b
PrismSpirit <yeolkeut@gmail.com>
2020-07-07 20:23:39 +0900

    structure of Stack

---
+++ Stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        pass
+
+    def size(self):
+        pass
+
+    def isEmpty(self):
+        pass
+
+    def push(self, e):
+        pass
+
+    def top(self):
+        pass
+
+    def pop(self):
+        pass
